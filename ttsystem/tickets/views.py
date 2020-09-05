c4f930784e0b1c135e938360c26425ca5825ee4a
Jeff Gruenbaum <jgruenbaum@teleflora.org>
2020-09-05 13:48:52 -0500

    updated views

--- ttsystem/tickets/views.py
+++ ttsystem/tickets/views.py
@@ -1,13 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, Http404
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import Ticket
 
 # Create your views here.
 def index(request):
     latest_ticket_list = Ticket.objects.all()
-    output = ', '.join([t.ticketType.ticketType + '-' + str(t.id) for t in latest_ticket_list])
-    return HttpResponse(output)
+    context = {'latest_ticket_list': latest_ticket_list}
+    # output = ', '.join([t.ticketType.ticketType + '-' + str(t.id) for t in latest_ticket_list])
+    return render(request, 'tickets/index.html', context)
 
 def detail(request, ticket_id):
-    return HttpResponse("You're looking at question %s." % ticket_id)
+    ticket = get_object_or_404(Ticket, pk=ticket_id)
+    return render(request, 'tickets/detail.html', {'ticket': ticket})

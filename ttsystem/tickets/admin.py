93c1a032733063bf81f07adb47ea6c1172cb7cb7
Jeff Gruenbaum <jgruenbaum@teleflora.org>
2020-09-04 21:26:51 -0500

    models, excluding users, are setup for ttsystem

--- ttsystem/tickets/admin.py
+++ ttsystem/tickets/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import TicketType, Project, Priority, Status, Ticket, Attachment, Comment
 # Register your models here.
+admin.site.register(TicketType)
+admin.site.register(Project)
+admin.site.register(Priority)
+admin.site.register(Status)
+admin.site.register(Ticket)
+admin.site.register(Attachment)
+admin.site.register(Comment)

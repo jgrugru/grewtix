9f3d33cf3dc052c06f4e67c574181be0ffea6e55
Jeff Gruenbaum <jgruenbaum@teleflora.org>
2020-09-22 09:31:10 -0500

    moved footer/header to design dir. minor changes to views.py

--- ttsystem/tickets/urls.py
+++ ttsystem/tickets/urls.py
@@ -5,7 +5,6 @@ from . import views
 app_name = 'tickets'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    # path('create/', views.ticket_create_view, name='create'),
     path('create/', views.TicketCreate.as_view(), name='create'),
     path('edit/<int:pk>', views.TicketUpdate.as_view(), name='edit'),
     # path('<int:ticket_id>/', views.ticket_edit_view, name='edit')
--- ttsystem/tickets/views.py
+++ ttsystem/tickets/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import get_object_or_404, render, Http404
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import get_object_or_404
 from django.views import generic 
 from django.urls import reverse
 from django.contrib.auth.models import User
@@ -18,41 +16,17 @@ class IndexView(generic.ListView):
         """Return the last five published Tickets."""
         return Ticket.objects.order_by('-created_at')[:5]
 
-def ticket_edit_view(request, ticket_id):
-    ticket = get_object_or_404(Ticket, pk=ticket_id)
-    data = {
-        'ticketType': ticket.ticketType,
-        'subject': ticket.subject,
-        'project': ticket.project,
-        'description': ticket.description,
-        'priority': ticket.priority,
-        'owner': ticket.owner,
-        'status': ticket.status,
-        # 'creation_date': ticket.created_at,
-        # 'creator': ticket.creator,
-    }
-    form = TicketForm(data)
-    return render(request, 'tickets/detail.html', {'form': form, 'ticket':ticket})
-    # return render(request, 'tickets/detail.html', {'ticket': ticket})             #this can be used for users with only read permissions
-
-def ticket_create_view(request):
-    form = TicketForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        form = TicketForm()
-
-    user = get_object_or_404(User, pk=1)
-    return render(request, 'tickets/ticket_create.html', {'form': form, 'user': user})
-
-
 class TicketCreate(CreateView):
+    template_name = 'tickets/ticket_create_form.html'
     model = Ticket
-    fields = ['ticketType', 'subject', 'project', 'description', 'priority', 'owner', 'status']
-
+    form_class = TicketForm
+    
+    def get_success_url(self):
+        return reverse('tickets:index')
 
 class TicketUpdate(UpdateView):
     model = Ticket 
-    fields = ['ticketType', 'subject', 'project', 'description', 'priority', 'owner', 'status']
+    form_class = TicketForm
     template_name_suffix = '_update_form'
 
     def get_success_url(self):
@@ -64,4 +38,31 @@ class TicketUpdate(UpdateView):
 
 ## create attachment action
 
+
+
+# def ticket_edit_view(request, ticket_id):
+#     ticket = get_object_or_404(Ticket, pk=ticket_id)
+#     data = {
+#         'ticketType': ticket.ticketType,
+#         'subject': ticket.subject,
+#         'project': ticket.project,
+#         'description': ticket.description,
+#         'priority': ticket.priority,
+#         'owner': ticket.owner,
+#         'status': ticket.status,
+#         # 'creation_date': ticket.created_at,
+#         # 'creator': ticket.creator,
+#     }
+#     form = TicketForm(data)
+#     return render(request, 'tickets/detail.html', {'form': form, 'ticket':ticket})
+    # return render(request, 'tickets/detail.html', {'ticket': ticket})             #this can be used for users with only read permissions
+
+# def ticket_create_view(request):
+#     form = TicketForm(request.POST or None)
+#     if form.is_valid():
+#         form.save()
+#         form = TicketForm()
+
+#     user = get_object_or_404(User, pk=1)
+#     return render(request, 'tickets/ticket_create.html', {'form': form, 'user': user})
 

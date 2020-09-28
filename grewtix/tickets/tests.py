5d7a5ae5b16fdcc5244e021791aed4541f15decd
Jeff Gruenbaum <jeff.gruenbaum@eagles.oc.edu>
2020-09-28 17:41:45 -0500

    setting up ticket view testing

--- grewtix/tickets/tests.py
+++ grewtix/tickets/tests.py
@@ -1,10 +1,10 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from .models import Ticket, TicketType, Project
 from django.contrib.auth.models import User
 from model_bakery import baker
+from django.urls import reverse
 
 # Create your tests here.
-
 def create_ticketType():
     x =  baker.make('TicketType')
     return x
@@ -17,19 +17,20 @@ def create_user():
     x = baker.make('User')
     return x
 
-    
-def create_ticket():
-    x = baker.make('Ticket')
-    print(x.ticketType, x.subject, x.project, x.description, x.priority, x.status, x.creator, x.owner)
-    x = Ticket(ticketType=create_ticketType(), subject='test', project=create_project(), description='testing', priority='Critical', status='Backlog', creator=create_user())
-    print(x.ticketType, x.subject, x.project, x.description, x.priority, x.status, x.creator, x.owner)
+# class TicketModelTests(TestCase):
 
-    return x
+#     def create_ticket(self):
+#         x = baker.make('Ticket')
+#         # print(x.ticketType, x.subject, x.project, x.description, x.priority, x.status, x.creator, x.owner)
+#         return x
  
+#     def test_Baker(self):
+#         print(type(create_ticketType()))
+#         print(type(create_project()))
+#         print(type('ticket: ', self.create_ticket()))
 
-class TicketModelTests(TestCase):
-
-    def test_Baker(self):
-        print(type(create_ticketType()))
-        print(type(create_project()))
-        print(type(create_ticket()))
+class TicketIndexViewTests(TestCase):
+    def test_no_tickets(self):
+        response = self.client.get(reverse('tickets:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(response.context['latest_ticket_list'], [])

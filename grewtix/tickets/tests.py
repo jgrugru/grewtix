a7612e5ab0033194be27afd376fa7b3f1fbb5859
Jeff Gruenbaum <jeff.gruenbaum@eagles.oc.edu>
2020-09-28 17:27:27 -0500

    inital setup for ticketsmodel test. added new package model_bakery

--- grewtix/tickets/tests.py
+++ grewtix/tickets/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
-from .model import Ticket
+from .models import Ticket, TicketType, Project
+from django.contrib.auth.models import User
+from model_bakery import baker
+
 # Create your tests here.
+
+def create_ticketType():
+    x =  baker.make('TicketType')
+    return x
+
+def create_project():
+    x =  baker.make('Project')
+    return x
+
+def create_user():
+    x = baker.make('User')
+    return x
+
+    
+def create_ticket():
+    x = baker.make('Ticket')
+    print(x.ticketType, x.subject, x.project, x.description, x.priority, x.status, x.creator, x.owner)
+    x = Ticket(ticketType=create_ticketType(), subject='test', project=create_project(), description='testing', priority='Critical', status='Backlog', creator=create_user())
+    print(x.ticketType, x.subject, x.project, x.description, x.priority, x.status, x.creator, x.owner)
+
+    return x
+ 
+
+class TicketModelTests(TestCase):
+
+    def test_Baker(self):
+        print(type(create_ticketType()))
+        print(type(create_project()))
+        print(type(create_ticket()))

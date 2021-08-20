529c48f155758bc03e3b438633df197777e0314e
jgrugru <jeff.gruenbaum@gmail.com>
2021-08-20 18:32:44 -0500

    added authentication and permissions

--- tickets/views.py
+++ tickets/views.py
@@ -2,52 +2,53 @@ from django.shortcuts import render
 from django.views import generic
 from django.urls import reverse
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from tickets.models import Ticket
 from tickets.forms import TicketForm
+
 # Create your views here.
 
-
+@login_required
 def ticket_index(request):
     return render(request, 'ticket_index.html')
 
-
+@login_required
 def reports(request):
     return render(request, 'ticket_reports.html')
 
-
-class TicketListView(generic.ListView):
+class TicketListView(LoginRequiredMixin, generic.ListView):
     template_name = "ticket_display_queryset.html"
     context_object_name = 'ticket_list'
-
+    login_url = '/accounts/login/'
+    redirect_field_name = 'redirect_to'
 
 class RecentlyCreatedView(TicketListView):
     def get_queryset(self):
         """Return the last five published Tickets."""
         return Ticket.objects.order_by('-created_at')[:4]
 
-
 class OwnedByUserView(TicketListView):
     def get_queryset(self):
         return Ticket.objects.filter(owner=self.request.user.id).order_by('-created_at')
-
 
 class CreatedByUserView(TicketListView):
     def get_queryset(self):
         return Ticket.objects.filter(creator=self.request.user.id).order_by('-created_at')
 
-
 class UnassignedView(TicketListView):
     def get_queryset(self):
         return Ticket.objects.filter(owner=None).order_by('-created_at')
-
 
 class AllTicketsView(TicketListView):
     def get_queryset(self):
         return Ticket.objects.all().order_by('-created_at')
 
-class TicketFormView():
+class TicketFormView(LoginRequiredMixin):
     model = Ticket
     form_class = TicketForm
+    login_url = '/accounts/login/'
+    redirect_field_name = 'redirect_to'
 
     def get_success_url(self):
         return reverse('tickets:ticket_index')
